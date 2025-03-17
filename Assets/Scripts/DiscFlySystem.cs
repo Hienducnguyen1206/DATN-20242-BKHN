03730239e80ce8ef73fc5d5bb28826bd93ad8d75
Nguyenduchien1206 <hiennd.nguyenduc2001@gmail.com>
2025-03-17 16:58:01 +0700

    init project

---
+++ Assets/Scripts/DiscFlySystem.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscFlySystem : MonoBehaviour
+{
+    [SerializeField] DiscStats discStats;
+    [SerializeField] Rigidbody discRb;
+    
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ Assets/Scripts/DiscStats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+    [CreateAssetMenu(fileName = "DISCSTATS", menuName = "DISCSTATS", order = 0)]
+    public class DiscStats : ScriptableObject
+    {
+        [Header("Disc Stats")]
+        [Range(1, 14)] public int Speed;
+        [Range(1, 7)] public int Glide;
+        [Range(-5,1)] public int Turn;
+        [Range(0,5)] public int Fade;
+    }
